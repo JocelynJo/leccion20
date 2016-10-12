41749b7968be875a705b186dad4ec901900d3755
JocelynJoe <jocelyn.pena.contreras@gmail.com>
2016-10-12 14:48:23 -0700

    primer commit java script

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function callbackPromedio(a,b,c,callback){
+	var prom = (a+b+c)/3;
+	return callback(prom);
+
+}
+
+var tiempo=1;
+
+function segundero(){
+	
+	document.getElementById("titulo").innerHTML = tiempo;
+	tiempo++;
+}
+setInterval(segundero,1000);
+
